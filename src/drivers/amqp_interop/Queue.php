995c1df868a293237d15a1689afaf13f4f9486ba
xyq <xyq@weiyian.com>
2021-02-24 10:31:09 +0800

    删除无用组件及优化代码

--- src/drivers/amqp_interop/Queue.php
+++ src/drivers/amqp_interop/Queue.php
@@ -255,7 +255,7 @@ class Queue extends CliQueue
     {
         parent::init();
         Event::on(BaseApp::class, BaseApp::EVENT_AFTER_REQUEST, function () {
-            $this->close();
+            $this->close('all');
         });
     }
 
