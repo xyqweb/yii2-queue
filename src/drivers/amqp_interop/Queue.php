7f5308f9b5dd72158647b9925685c43d5a223284
xyq <xyq@weiyian.com>
2021-12-21 15:30:12 +0800

    调整消费端超时时间

--- src/drivers/amqp_interop/Queue.php
+++ src/drivers/amqp_interop/Queue.php
@@ -296,15 +296,16 @@ class Queue extends CliQueue
                     }
                     return true;
                 });
-                $subscriptionConsumer->consume();
+                $subscriptionConsumer->consume(300000);
             } catch (\Exception $exception) {
             } catch (\Throwable $exception) {
+            } finally {
+                $this->close('consumer');
+                sleep(1);
             }
             if (!is_null($exception)) {
                 $this->logDriver->write('queue/queue_consumer.log', 'consumer process error ,restart in 1 second, error message:' . $exception->getMessage() . ',file:' . $exception->getFile() . ',line:' . $exception->getLine());
                 $exception = null;
-                $this->close('consumer');
-                sleep(1);
             }
         }
     }
